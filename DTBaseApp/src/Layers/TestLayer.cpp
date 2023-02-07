e3e51a97051dfd0a09b9bc489809972aeed91269
Adrian Cozac <electro_ady@LIVE.IT>
2023-02-07 22:09:16 +0100

    Some demo

--- DTBaseApp/src/Layers/TestLayer.cpp
+++ DTBaseApp/src/Layers/TestLayer.cpp
@@ -14,6 +14,97 @@ namespace DT
 
 	void TestLayer::OnEvent(Event& event)
 	{
+		Event::Dispatcher dispatcher(event);
+		dispatcher.Dispatch<KeyPressedEvent>([&](KeyPressedEvent& key)
+		{
+			switch (key.GetKeyCode())
+			{
+				case Key::M:
+				{
+					Application::Get().GetWindow().Maximize();
+					break;
+				}
+				case Key::F:
+				{
+					static bool windowed = true;
+					if (windowed)
+						Application::Get().GetWindow().ToFullscreen();
+					else
+						Application::Get().GetWindow().ToWindowed();
+					windowed = !windowed;
+					break;
+				}
+				case Key::A:
+				{
+					static bool fixed = false;
+					fixed = !fixed;
+					if (fixed)
+						Application::Get().GetWindow().SetFixedAspectRatio(16, 9);
+					else
+						Application::Get().GetWindow().SetFixedAspectRatio(-1, -1);
+					break;
+				}
+				case Key::C:
+				{
+					Application::Get().GetWindow().SetMousePosition(200, 200);
+					break;
+				}
+				case Key::U:
+				{
+					LOG_TRACE(Application::Get().GetWindow().GetClipboardString());
+					break;
+				}
+				case Key::Up:
+				{
+					m_Opacity += 0.1f;
+					m_Opacity = std::clamp(m_Opacity, 0.0f, 1.0f);
+					Application::Get().GetWindow().SetOpacity(m_Opacity);
+					break;
+				}
+				case Key::Down:
+				{
+					m_Opacity -= 0.1f;
+					m_Opacity = std::clamp(m_Opacity, 0.0f, 1.0f);
+					Application::Get().GetWindow().SetOpacity(m_Opacity);
+					break;
+				}
+				case Key::T:
+				{
+					Application::Get().GetWindow().SetTitle("New title!");
+					break;
+				}
+				case Key::D:
+				{
+					static bool decorated = true;
+					decorated = !decorated;
+					Application::Get().GetWindow().SetDecorated(decorated);
+					break;
+				}
+				case Key::R:
+				{
+					static bool resizable = true;
+					resizable = !resizable;
+					Application::Get().GetWindow().SetResizable(resizable);
+					break;
+				}
+				case Key::S:
+				{
+					Application::Get().GetWindow().CenterWindow();
+					break;
+				}
+				case Key::N:
+				{
+					Application::Get().GetWindow().SetSizeLimits(200, 200, 500, 300);
+					break;
+				}
+			}
+			return false;
+		});
+		dispatcher.Dispatch<WindowFocusEvent>([](WindowFocusEvent& e) 
+		{
+			LOG_TRACE("Window focused? {}", e.IsFocused());
+			return false;
+		});
 	}
 
 	void TestLayer::OnDetach()
--- DTBaseApp/src/Platform/WindowsWindow.h
+++ DTBaseApp/src/Platform/WindowsWindow.h
@@ -11,22 +11,22 @@ namespace DT
 		WindowsWindow(const WindowSpecification& specification);
 		virtual ~WindowsWindow() override;
 
+		virtual void SetEventCallBack(const EventCallbackFn& callback) override;
 		virtual const WindowSpecification& GetSpecification() const { return m_Specification; };
 
-		virtual void SetEventCallBack(const EventCallbackFn& callback) override;
 		virtual void ProcessEvents() override;
-
-		virtual int32 GetWidth() const override;
-		virtual int32 GetHeight() const override;
 		virtual void Maximize() override;
+		virtual void CenterWindow() override;
 		virtual void ToFullscreen() override;
 		virtual void ToWindowed() override;
-		virtual void SetFixedAspectRatio(int32 numerator, int32 denominator) override;
+		virtual int32 GetWidth() const override;
+		virtual int32 GetHeight() const override;
 		virtual int32 GetMouseX() const override;
 		virtual int32 GetMouseY() const override;
 		virtual std::string GetClipboardString() const override;
 		virtual Extent GetDisplayResolution() const override;
 
+		virtual void SetFixedAspectRatio(int32 numerator, int32 denominator) override;
 		virtual void SetMousePosition(int32 x, int32 y) override;
 		virtual void SetOpacity(float opacityValue) override;
 		virtual void SetTitle(const std::string& title) override;
@@ -34,7 +34,6 @@ namespace DT
 		virtual void SetResizable(bool isResizable) override;
 		virtual void SetSize(int32 width, int32 height) override;
 		virtual void SetPosition(int32 x, int32 y) override;
-		virtual void CenterWindow() override;
 		virtual void SetSizeLimits(int32 minWidth, int32 minHeight, int32 maxWidth, int32 maxHeight) override;
 		virtual void SetIcon(const std::filesystem::path& iconPath) override;
 
