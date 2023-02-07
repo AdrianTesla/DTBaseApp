c4785c033dde2d8e4391f1947bb9670356e21f4a
Adrian Cozac <electro_ady@LIVE.IT>
2023-02-07 22:24:39 +0100

    stb_build

--- vendor/stb/stb_build.cpp
+++ vendor/stb/stb_build.cpp
@@ -1,5 +1,6 @@
 #pragma warning(push, 0)
 
+
 #define STB_IMAGE_IMPLEMENTATION
 #include <stb_image.h>
 
@@ -11,5 +12,6 @@
 
 #define STB_INCLUDE_IMPLEMENTATION
 #include <stb_include.h>
+
 
 #pragma warning(pop)
